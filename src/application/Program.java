9ccd7f51324ffc76be8b0c5156bacfcae8c8cb86
Guilherme Gomes <barbozaguilherme717@gmail.com>
2020-06-30 10:30:57 -0300

    add class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import BoardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("teste inicial");
+		
+		Position pos = new Position(5,6);
+		System.out.println(pos);
 	}
 
 }
